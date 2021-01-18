04bd2e0741bf14fdb10d3f07117b222590dd7d81
aderibigbeganiu <aderibigbeganiu@gmail.com>
2021-01-18 15:01:58 +0100

    dependences updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>hello there</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<p>hello there</p>
+		</div>
+	);
 }
 
 export default App;
